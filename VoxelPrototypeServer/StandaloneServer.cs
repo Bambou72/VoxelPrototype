491006fb39e2d9ffe3b6dafcb8d4ea28a030c161
Bambou72 <82939192+Bambou72@users.noreply.github.com>
2024-03-26 19:59:56 +0100

    Begin to write standalone server

---
+++ VoxelPrototypeServer/StandaloneServer.cs
@@ -0,0 +1,74 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using VoxelPrototype.common.Game.World;
+using VoxelPrototype.common.Game;
+using VoxelPrototype.common.Network.server;
+using VoxelPrototype.common.RessourceManager;
+
+namespace VoxelPrototypeServer
+{
+    internal class StandaloneServer
+    {
+        internal WorldSettings InitialSettings;
+        internal RessourcePackManager RessourcePackManager;
+        internal static string Title = @"
+         __      __           _ _____           _        _                       _____                          
+         \ \    / /          | |  __ \         | |      | |                     / ____|                         
+          \ \  / /____  _____| | |__) | __ ___ | |_ ___ | |_ _   _ _ __   ___  | (___   ___ _ ____   _____ _ __ 
+           \ \/ / _ \ \/ / _ \ |  ___/ '__/ _ \| __/ _ \| __| | | | '_ \ / _ \  \___ \ / _ \ '__\ \ / / _ \ '__|
+            \  / (_) >  <  __/ | |   | | | (_) | || (_) | |_| |_| | |_) |  __/  ____) |  __/ |   \ V /  __/ |   
+             \/ \___/_/\_\___|_|_|   |_|  \___/ \__\___/ \__|\__, | .__/ \___| |_____/ \___|_|    \_/ \___|_|   
+                                                              __/ | |                                           
+                                                             |___/|_|                                           
+        ";
+        public StandaloneServer() : base()
+        {
+            //Ressources
+            RessourcePackManager = new();
+            //InitialSettings = Settings;
+            //this.Path = Path;
+        }
+        public void Run()
+        {
+            Console.WriteLine(Title);
+
+            //World = new(Settings, Path);
+            //ServerNetwork.StartServer(ServerPort);
+            //Logger.Info("The server has finished initializing, it is now ready at: " + ServerNetwork.server.LocalPort);
+            //Logger.Info("Server engine version: " + Version.EngineVersion);
+            //Logger.Info("Server api version: " + Version.APIVersion);
+            /*while (Running)
+            {
+                if (TickCounter < 20)
+                {
+                    Stopwatch.Start();
+                    ServerNetwork.Update();
+                    World.Tick();
+                    TickCounter++;
+                    Stopwatch.Stop();
+                }
+                else
+                {
+                    TickCounter = 0;
+                }
+                double elapsed = Stopwatch.ElapsedMilliseconds;
+                if (elapsed < 50)
+                {
+
+                    Thread.Sleep(50 - (int)elapsed);
+                }
+                else
+                {
+                    Logger.Warn("Tick can't run at 20tps, current time for one tick :" + elapsed);
+                }
+                Stopwatch.Reset();
+            }*/
+            //World.Dispose();
+           // ServerNetwork.StopServer();
+
+
+        }
+    }
+}

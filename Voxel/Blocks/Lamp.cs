c7a3c5af05cf77309fa0fdc035d0ebe0ce46cba0
Bambou72 <82939192+Bambou72@users.noreply.github.com>
2024-03-28 13:05:52 +0100

    Add new block to test blockstate

---
+++ Voxel/Blocks/Lamp.cs
@@ -0,0 +1,34 @@
+﻿using OpenTK.Mathematics;
+using VoxelPrototype.client;
+using VoxelPrototype.common.API.Blocks;
+using VoxelPrototype.common.API.Blocks.Properties;
+using VoxelPrototype.common.API.Blocks.state;
+using VoxelPrototype.common.API.Blocks.State;
+
+namespace Voxel.Blocks
+{
+    internal class Lamp : Block
+    {
+        BooleanProperty Lit = new("Lit");
+        public Lamp()
+        {
+        }
+        public override void RegisterProperties(BlockStateBuilder Builder)
+        {
+            base.RegisterProperties(Builder);
+            Builder.Register(Lit);
+        }
+        public override void OnInteract(Vector3i Pos, BlockState State)
+        {
+            base.OnInteract(Pos, State);
+            if(State.Get(new BooleanProperty("Lit")) ==  true)
+            {
+                Client.TheClient.World.ChunkManager.ChangeChunk(Pos,State.With(new BooleanProperty("Lit"),false));
+            }
+            else
+            {
+                Client.TheClient.World.ChunkManager.ChangeChunk(Pos, State.With(new BooleanProperty("Lit"), true));
+            }
+        }
+    }
+}

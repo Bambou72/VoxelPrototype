2ca34f60fe4e0044f3c71f48d65443de71600e30
Bambou72 <82939192+Bambou72@users.noreply.github.com>
2024-03-30 22:45:51 +0100

    Remove unused code

--- VoxelPrototype/common/Game/World/WorldInformations.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VoxelPrototype.common.Game.World
-{
-    internal class WorldInformations
-    {
-        internal string Path;
-        internal string Name;
-        internal long Seed;
-    }
-}

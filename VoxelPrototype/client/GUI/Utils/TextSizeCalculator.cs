421e181d9909b1d385f30317e723aa48483a065e
Bambou72 <82939192+Bambou72@users.noreply.github.com>
2024-05-20 10:12:38 +0200

    Fix : move textsizecalculator to ui utils

---
+++ VoxelPrototype/client/GUI/Utils/TextSizeCalculator.cs
@@ -0,0 +1,32 @@
+﻿using VoxelPrototype.client.Render.Text;
+namespace VoxelPrototype.client.GUI.Utils
+{
+    public static class TextSizeCalculator
+    {
+        public static float CalculateSize(string Font, float Scale, string Text)
+        {
+            var RealFont = Client.TheClient.ResourcePackManager.GetFont(Font);
+
+            Scale = Scale / RealFont.FontSize;
+            float size = 0;
+            for (int i = 0; i < Text.Length; i++)
+            {
+                size += (RealFont.GetCharacter(Text[i]).Advance >> 6) * Scale;
+            }
+            return size;
+        }
+        public static float CalculateVerticalSize(string Font, float Scale, string Text)
+        {
+            var RealFont = Client.TheClient.ResourcePackManager.GetFont(Font);
+            Scale = Scale / RealFont.FontSize;
+
+            float size = 0;
+            for (int i = 0; i < Text.Length; i++)
+            {
+                var ch = RealFont.GetCharacter(Text[i]);
+                size = Math.Max(size, ch.Size.Y * Scale);
+            }
+            return size;
+        }
+    }
+}

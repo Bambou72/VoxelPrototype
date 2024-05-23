7eff495288f70e2d336a18c84a91f7fe102de5c7
Bambou72 <82939192+Bambou72@users.noreply.github.com>
2024-05-23 12:09:38 +0200

    Feat : Add custom version

---
+++ VoxelPrototype/Version.cs
@@ -0,0 +1,48 @@
+﻿
+namespace VoxelPrototype
+{
+    public enum VersionType
+    {
+        Release,
+        ReleaseCandidate,
+        Dev
+    }
+    public class Version : IEquatable<Version>
+    {
+        public Version( VersionType Type, int major, int minor, int patch)
+        {
+            this.Type = Type;
+            Major = major;
+            Minor = minor;
+            Patch = patch;
+        }
+        VersionType Type;
+        public int Major { get; set; } = 0;
+        public int Minor { get; set; } = 1;
+        public int Patch { get; set; } = 0;
+
+
+        public bool Equals(Version? other)
+        {
+            if(Major != other.Major || Minor != other.Minor|| Patch != other.Patch)
+            {
+                return false;
+            }
+            return true;
+        }
+        public override string ToString()
+        {
+            string suf = "";
+            if(Type == VersionType.ReleaseCandidate)
+            {
+                suf = "rc";
+
+            }else if(Type == VersionType.Dev)
+            {
+                suf = "d";
+            }
+
+            return Major+ "." + Minor+"."+suf+Patch;
+        }
+    }
+}

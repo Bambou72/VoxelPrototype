526d49fe077b7bf63198cb81ed540712b166c19d
Bambou72 <82939192+Bambou72@users.noreply.github.com>
2024-05-26 19:17:16 +0200

    Feat : Resources Manager utils

---
+++ VoxelPrototype/common/Utils/ListExtensions.cs
@@ -0,0 +1,53 @@
+﻿namespace VoxelPrototype.common.Utils
+{
+    public static class ListExtensions
+    {
+        /// <summary>
+        /// Moves an object in the list to the first position.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the list.</typeparam>
+        /// <param name="list">The list to modify.</param>
+        /// <param name="item">The object to move to the first position.</param>
+        public static void MoveToFirstPosition<T>(this List<T> list, T item)
+        {
+            int index = list.IndexOf(item);
+            if (index != -1)
+            {
+                list.RemoveAt(index);
+                list.Insert(0, item);
+            }
+        }
+
+        /// <summary>
+        /// Moves an object in the list up by one position.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the list.</typeparam>
+        /// <param name="list">The list to modify.</param>
+        /// <param name="item">The object to move up.</param>
+        public static void MoveUp<T>(this List<T> list, T item)
+        {
+            int index = list.IndexOf(item);
+            if (index > 0)
+            {
+                list.RemoveAt(index);
+                list.Insert(index - 1, item);
+            }
+        }
+
+        /// <summary>
+        /// Moves an object in the list down by one position.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the list.</typeparam>
+        /// <param name="list">The list to modify.</param>
+        /// <param name="item">The object to move down.</param>
+        public static void MoveDown<T>(this List<T> list, T item)
+        {
+            int index = list.IndexOf(item);
+            if (index != -1 && index < list.Count - 1)
+            {
+                list.RemoveAt(index);
+                list.Insert(index + 1, item);
+            }
+        }
+    }
+}
---
+++ VoxelPrototype/common/Utils/RelativePath.cs
@@ -0,0 +1,43 @@
+﻿namespace VoxelPrototype.common.Utils
+{
+    public static class RelativePath
+    {
+        public static string[] GetRelativePathsDirectories(string baseDir)
+        {
+            string[] directories = Directory.GetDirectories(baseDir);
+            string[] relativePaths = new string[directories.Length];
+
+            for (int i = 0; i < directories.Length; i++)
+            {
+                relativePaths[i] = GetRelativePath(baseDir, directories[i]);
+            }
+
+            return relativePaths;
+        }
+        public static string GetRelativePathFile(string baseDir, string fullPath)
+        {
+            string absoluteBaseDir = Path.GetFullPath(baseDir);
+            string absoluteFullPath = Path.GetFullPath(fullPath);
+            if (!absoluteFullPath.StartsWith(absoluteBaseDir, StringComparison.OrdinalIgnoreCase))
+            {
+                throw new InvalidOperationException("Le chemin complet ne commence pas par le répertoire de base.");
+            }
+            string relativePath = absoluteFullPath.Substring(absoluteBaseDir.Length).TrimStart(Path.DirectorySeparatorChar);
+            string directory = Path.GetDirectoryName(relativePath);
+            string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(relativePath);
+            string specificPart = Path.Combine(directory, fileNameWithoutExtension);
+            return specificPart.Replace(Path.DirectorySeparatorChar, '/');
+        }
+        public static string GetRelativePath(string baseDir, string fullPath)
+        {
+            if (!fullPath.StartsWith(baseDir))
+            {
+                throw new InvalidOperationException();
+            }
+
+            string relativePath = fullPath.Replace(baseDir,"");
+            relativePath = relativePath.Replace("\\","");
+            return relativePath;
+        }
+    }
+}
